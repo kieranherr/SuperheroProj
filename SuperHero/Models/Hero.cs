1a4eb431996824db2fbdd9304b73e203be844948
Kieran Herr <kieranherr@gmail.com>
2020-04-27 16:02:44 -0500

    Didn't realize I didn't make one

---
+++ SuperHero/Models/Hero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHero.Models
+{
+    public class Hero
+    {
+        [Key]
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimeAbility { get; set; }
+        public string SecondAbility { get; set; }
+        public string CatchPhrase { get; set; }
+
+
+    }
+}
